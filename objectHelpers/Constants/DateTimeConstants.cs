f1042d0d8bfb65bc884a7121e7cc24dc1d7a9cfb
@sstone-loveall <stone.sarah.l@gmail.com>
2018-04-04 13:56:33 -0400

    Object Helpers class library

--- objectHelpers/Constants/DateTimeConstants.cs
+++ objectHelpers/Constants/DateTimeConstants.cs
@@ -6,7 +6,7 @@
 	public static class DateTimeConstants
 	{
 		/// <summary>
-		/// SQL DateTime formatting string.
+		/// DateTime formatting string to include milliseconds.
 		/// </summary>
 		public const string DATE_TIME_WITH_MILLISECONDS_FORMAT = "MM/dd/yyyy HH:mm:ss:fff";
 	}
